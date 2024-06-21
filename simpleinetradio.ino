9fefa2ba5a9ab4f246b26ed823ecbd907f7ba7ed
sergeyhaki <navent@mail.ru>
2024-06-21 19:54:08 +0300

    Update simpleinetradio.ino

--- simpleinetradio.ino
+++ simpleinetradio.ino
@@ -1,9 +1,9 @@
 //this file: simpleinetradio.ino
 #include "Audio.h" //see repository at github "https://github.com/schreibfaul1/ESP32-audioI2S"
 
-#define I2S_DOUT      25
-#define I2S_BCLK      27
-#define I2S_LRC       26
+#define I2S_DOUT      25 // connect to PCM5102
+#define I2S_BCLK      27 // connect to PCM5102
+#define I2S_LRC       26 // connect to PCM5102
 
 //global vars
 Audio audio;
@@ -17,7 +17,7 @@ void setup() {
   WiFi.begin(ssid.c_str(), password.c_str());
   audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
   audio.setVolume(21); // 0...21
-  audio.connecttohost(stations);
+  audio.connecttohost(stations.c_str());
 }
 
 void loop() {
